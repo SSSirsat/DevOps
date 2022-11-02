c5520606f9a90b5e10910336ad11769369793bda
Shubham Suryakant Sirsat <shubham.sirsat321@gmail.com>
2022-11-02 16:38:40 +0530

    sss

--- Kubernetes/Kubernetes cluster project/app.py
+++ Kubernetes/Kubernetes cluster project/app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
+import socket
 import os
 
 app = Flask(__name__)
 
+hostname=socket.gethostname()   
+IPAddr=socket.gethostbyname(hostname) 
+
 @app.route("/")
 def user():
     return render_template("index.html")
+
 @app.route("/login")
-def login():
-    return render_template("login.html")
-@app.route("/logout")
-def logout():
-    return render_template("logout.html")
+def ipfind():
+    return render_template("about.html", IP=IPAddr, HNAME=hostname)
 
 if __name__ == '__main__':
     # app.run(debug=True)
